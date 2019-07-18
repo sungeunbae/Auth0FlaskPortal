a4849703b3becfb122fbd1514e5405c55614c1e6
Sung Eun Bae <sung.bae@canterbury.ac.nz>
2019-07-18 04:46:16 +0000

    Updated requirements.txt

--- run_server.sh
+++ run_server.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-export PYTHONPATH=$PYTHONPATH:/home/seb56/local/app_dispatch:/home/seb56/local/app_dispatch/apps/devel/seistech:/home/seb56/local/app_dispatch/apps/devel/seistech/seistech_web
-#export PYTHONPATH=$PYTHONPATH:/home/seb56/local/app_dispatch:/home/seb56/local/app_dispatch/apps/devel
-/home/seb56/py36_devel/bin/uwsgi --http 0.0.0.0:3000 --http-websockets --plugins python,gevent --gevent 100 --mount /=server:main_app --master -b 8192 --logger file:logfile=logs/uwsgi.log,maxsize=200000 #32768
-#/home/seb56/py36_devel/bin/uwsgi --http 0.0.0.0:3001 --http-websockets --mount /=server:main_app --master -b 8192 #32768
-
+export ROOT=/home/ec2-user/Auth0FlaskPortal
+export PYTHONPATH=$PYTHONPATH:$ROOT:$ROOT/apps/devel/seistech:$ROOT/apps/devel/seistech/seistech_web
+#export PYTHONPATH=$PYTHONPATH:$ROOT:$ROOT/apps/devel
+/home/ec2-user/py3env/bin/uwsgi --http 0.0.0.0:5000 --http-websockets --plugins python37,gevent --gevent 1000 --mount /=server:main_app --master -b 8192 --logger file:logfile=logs/uwsgi.log,maxsize=200000 --socket /run/seistech.sock --chmod-socket=777 --vacuum --die-on-term #32768
 
 
